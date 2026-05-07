477259eb2b5933083b71f99f2f0685d8f0ffe0ba
Benjamin Ummenhofer <benjamin.ummenhofer@intel.com>
2026-05-07 05:00:07 -0700

    remove deprecation warning

--- kernelfoundry/custom_test.py
+++ kernelfoundry/custom_test.py
@@ -3,24 +3,10 @@
 The canonical base class is now `kernelfoundry.test_base.TestBase`.
 """
 
-from __future__ import annotations
-
-import warnings
-
 from .test_base import TestBase
 
 __all__ = ["CustomTest", "TestBase"]
 
 
-class _CustomTestDeprecationProxy(type):
-    def __getattribute__(cls, name: str):
-        warnings.warn(
-            "`kernelfoundry.custom_test.CustomTest` is deprecated; use `kernelfoundry.test_base.TestBase` instead.",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        return super().__getattribute__(name)
-
-
-class CustomTest(TestBase, metaclass=_CustomTestDeprecationProxy):
+class CustomTest(TestBase):
     """Deprecated alias of `kernelfoundry.test_base.TestBase`."""
